8eb7be1d54fc9fb08a9f8c17ca77c68559d81285
Vikram Choudhary <vikramus4@gmail.com>
2024-11-05 16:15:32 -0500

    Part 1a implemented : AutomatonImpl , CompilerFrontendImpl

--- src/AutomatonImpl.java
+++ src/AutomatonImpl.java
@@ -7,7 +7,11 @@ public class AutomatonImpl implements Automaton {
     class StateLabelPair {
         int state;
         char label;
-        public StateLabelPair(int state_, char label_) { state = state_; label = label_; }
+
+        public StateLabelPair(int state_, char label_) {
+            state = state_;
+            label = label_;
+        }
 
         @Override
         public int hashCode() {
@@ -30,42 +34,62 @@ public class AutomatonImpl implements Automaton {
         start_states = new HashSet<Integer>();
         accept_states = new HashSet<Integer>();
         transitions = new HashMap<StateLabelPair, HashSet<Integer>>();
+        current_states = new HashSet<>();
     }
 
     @Override
     public void addState(int s, boolean is_start, boolean is_accept) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        if (is_start) {
+            start_states.add(s);
+        }
+        if (is_accept) {
+            accept_states.add(s);
+        }
     }
 
     @Override
     public void addTransition(int s_initial, char label, int s_final) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        StateLabelPair key = new StateLabelPair(s_initial, label);
+        transitions.computeIfAbsent(key, k -> new HashSet<>()).add(s_final);
     }
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        current_states.clear();
+        current_states.addAll(start_states);
     }
 
     @Override
     public void apply(char input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        HashSet<Integer> next_states = new HashSet<>();
+        for (int state : current_states) {
+            StateLabelPair key = new StateLabelPair(state, input);
+            if (transitions.containsKey(key)) {
+                next_states.addAll(transitions.get(key));
+            }
+        }
+        current_states = next_states;
     }
 
     @Override
     public boolean accepts() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        for (int state : current_states) {
+            if (accept_states.contains(state)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean hasTransitions(char label) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasTransitions'");
+        for (int state : current_states) {
+            StateLabelPair key = new StateLabelPair(state, label);
+            if (transitions.containsKey(key)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/CompilerFrontendImpl.java
+++ src/CompilerFrontendImpl.java
@@ -20,8 +20,75 @@ public class CompilerFrontendImpl extends CompilerFrontend {
      */
     @Override
     protected void init_lexer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init_lexer'");
+        LexerImpl lexer = new LexerImpl();
+
+        // NUM: [0-9]*\\.[0-9]+
+        AutomatonImpl numAutomaton = new AutomatonImpl();
+        int startNum = 0, decimalNum = 1, fractionNum = 2;
+        numAutomaton.addState(startNum, true, false);
+        numAutomaton.addState(decimalNum, false, false);
+        numAutomaton.addState(fractionNum, false, true);
+
+        for (char c = '0'; c <= '9'; c++) {
+            numAutomaton.addTransition(startNum, c, startNum);
+            numAutomaton.addTransition(decimalNum, c, fractionNum);
+            numAutomaton.addTransition(fractionNum, c, fractionNum);
+        }
+        numAutomaton.addTransition(startNum, '.', decimalNum);
+        lexer.add_automaton(TokenType.NUM, numAutomaton);
+
+        // PLUS: +
+        AutomatonImpl plusAutomaton = new AutomatonImpl();
+        plusAutomaton.addState(0, true, false);
+        plusAutomaton.addState(1, false, true);
+        plusAutomaton.addTransition(0, '+', 1);
+        lexer.add_automaton(TokenType.PLUS, plusAutomaton);
+
+        // MINUS: -
+        AutomatonImpl minusAutomaton = new AutomatonImpl();
+        minusAutomaton.addState(0, true, false);
+        minusAutomaton.addState(1, false, true);
+        minusAutomaton.addTransition(0, '-', 1);
+        lexer.add_automaton(TokenType.MINUS, minusAutomaton);
+
+        // TIMES: *
+        AutomatonImpl timesAutomaton = new AutomatonImpl();
+        timesAutomaton.addState(0, true, false);
+        timesAutomaton.addState(1, false, true);
+        timesAutomaton.addTransition(0, '*', 1);
+        lexer.add_automaton(TokenType.TIMES, timesAutomaton);
+
+        // DIV: /
+        AutomatonImpl divAutomaton = new AutomatonImpl();
+        divAutomaton.addState(0, true, false);
+        divAutomaton.addState(1, false, true);
+        divAutomaton.addTransition(0, '/', 1);
+        lexer.add_automaton(TokenType.DIV, divAutomaton);
+
+        // LPAREN: (
+        AutomatonImpl lparenAutomaton = new AutomatonImpl();
+        lparenAutomaton.addState(0, true, false);
+        lparenAutomaton.addState(1, false, true);
+        lparenAutomaton.addTransition(0, '(', 1);
+        lexer.add_automaton(TokenType.LPAREN, lparenAutomaton);
+
+        // RPAREN: )
+        AutomatonImpl rparenAutomaton = new AutomatonImpl();
+        rparenAutomaton.addState(0, true, false);
+        rparenAutomaton.addState(1, false, true);
+        rparenAutomaton.addTransition(0, ')', 1);
+        lexer.add_automaton(TokenType.RPAREN, rparenAutomaton);
+
+        // WHITE_SPACE: (' '|\n|\r|\t)
+        AutomatonImpl whitespaceAutomaton = new AutomatonImpl();
+        whitespaceAutomaton.addState(0, true, true);
+        whitespaceAutomaton.addTransition(0, ' ', 0);
+        whitespaceAutomaton.addTransition(0, '\n', 0);
+        whitespaceAutomaton.addTransition(0, '\r', 0);
+        whitespaceAutomaton.addTransition(0, '\t', 0);
+        lexer.add_automaton(TokenType.WHITE_SPACE, whitespaceAutomaton);
+
+        this.lex = lexer;
     }
 
 }
